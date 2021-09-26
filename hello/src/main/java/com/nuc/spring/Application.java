a808e7740493a0b10f63870e6988c97631bc5e54
wgtguotao <1472183290@qq.com>
2021-09-26 16:37:05 +0800

    first

---
+++ hello/src/main/java/com/nuc/spring/Application.java
@@ -0,0 +1,19 @@
+/*
+版权所有
+*/
+package com.nuc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 温国涛
+ * @version 1.0
+ * @date : 14:35 2021/9/26
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
